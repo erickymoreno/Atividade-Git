9ce5edb2cd18dcbd7df8b2af9ba5f122ab435cc0
Erickymoreno <erickymorenoads@gmail.com>
2022-01-18 10:23:26 -0400

    Escrevendo imagem com creatReadStream

--- atividades/node/api-rest-streaming/arquivos/uploadDeArquivos.js
+++ atividades/node/api-rest-streaming/arquivos/uploadDeArquivos.js
@@ -1,11 +1,6 @@
 const fs = require('fs')
 
 
-fs.readFile('./assets/dog1.jpg',(erro, buffer) => {
-    console.log('Imagem foi bufferizada com sucesso!')
-    console.log(buffer)
-
-    fs.writeFile('./assets/dog1-Copy.jpg', buffer, (erro) => { 
-        console.log("Imagem escrita com sucesso!")
-    })
-})
+fs.createReadStream('./assets/dog1.jpg')
+    .pipe(fs.createWriteStream('./assets/dog2.jpg'))
+    .on('finish', () => { console.log('Imagem escrita com sucesso!!!')})
