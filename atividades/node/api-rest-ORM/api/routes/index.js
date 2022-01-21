6e83d7db14b14754a8759d58f26632c157466f4c
Erickymoreno <erickymorenoads@gmail.com>
2022-01-21 06:53:37 -0400

    Modulo 6 - Controladores

--- atividades/node/api-rest-ORM/api/routes/index.js
+++ atividades/node/api-rest-ORM/api/routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const pessoas = require('./pessoasRoute')
+const niveis = require('./niveisRoute')
+const turmas = require('./turmasRoute')
 
-module.exports = app =>{
-    app.use(express.json());
-    app.use(pessoas);
+
+module.exports = app => {
+    app.use(
+        express.json(),
+        express.urlencoded({ extended: false }),
+        pessoas,
+        niveis,
+        turmas
+    )
 }
